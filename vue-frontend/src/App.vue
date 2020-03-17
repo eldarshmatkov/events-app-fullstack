9975e6c75054f416337079ead544bf7bae5df49f
Eldar Shmatkov <eldarshmatkov@gmail.com>
2020-03-17 18:35:14 +0300

    first architecture restructure

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Add Event</router-link> |
+      <router-link to="/search-event">Search Event</router-link>
     </div>
     <router-view/>
   </div>
---
+++ vue-frontend/src/views/AddEvent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="add-event">
+    <h1>This is an add-event page</h1>
+  </div>
+</template>
